322e31b2bc44e00f1f7ca74487749d690182cb0a
Alsu Rose <heyalsu@gmail.com>
2023-06-05 16:59:01 -0400

    fixed partially the npm issue, new play copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
+// const gulp = require('gulp');
+// const sass = require('gulp-sass');
+
+// sass.complier = require('node-sass');
+
+// const sasss = () => gulp
+//   .src('./src/scss/**/*.scss')
+//   .pipe(sass().on('error', sass.logError))
+//   .pipe(gulp.dest('./src/css'));
+
+// gulp.task('sass:watch', () => {
+//   gulp.watch('./src/scss/**/*.scss', ['sass'])
+// })
+
 const gulp = require('gulp');
-const sass = require('gulp-sass');
-sass.complier = require('node-sass');
+const sass = require('gulp-sass')(require('sass'));
 
-const sasss = () => gulp
-  .src('./src/scss/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./src/css'));
+function buildStyles() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+};
 
-gulp.task('sass:watch', () => {
-  gulp.watch('./src/scss/**/*.scss', sasss)
-})
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+};
