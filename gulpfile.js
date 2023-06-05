b530622731a900405e7cdc9c97c5d3744884accb
Alsu Rose <heyalsu@gmail.com>
2023-06-05 17:26:26 -0400

    fixed partially the npm issue, new play copy

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,35 @@
 
 // sass.complier = require('node-sass');
 
-// const sasss = () => gulp
-//   .src('./src/scss/**/*.scss')
-//   .pipe(sass().on('error', sass.logError))
-//   .pipe(gulp.dest('./src/css'));
-
-// gulp.task('sass:watch', () => {
-//   gulp.watch('./src/scss/**/*.scss', ['sass'])
-// })
-
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function buildStyles() {
-  return gulp.src('./src/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/css'));
-};
+const sasss = () => gulp
+  .src('./src/scss/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./src/css'));
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
-};
+gulp.task('sass:watch', () => {
+  gulp.watch('./src/scss/**/*.scss', sasss)
+})
+
+
+
+// function buildStyles() {
+//   return gulp.src('./src/scss/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./src/css'));
+// };
+
+// exports.buildStyles = buildStyles;
+// exports.watch = function () {
+//   gulp.watch('./src/scss/**/*.scss', ['sass']);
+// };
+
+// function buildStyles() {
+//   return gulp.src('./src/scss/**/*.scss')
+//     .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+//     .pipe(gulp.dest('./src/css'));
+// };
+
+// exports.buildStyles = buildStyles;
